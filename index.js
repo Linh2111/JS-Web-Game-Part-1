b0b9e007bb020e282dd7df3bd6fb14614144a731
Linh2111 <120156683+Linh2111@users.noreply.github.com>
2023-03-06 19:56:16 -0800

    complete

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+
+
+function newImage (image, left, bottom) {
+ 
+    let newElement = document.createElement('img')
+    newElement.src = image
+    newElement.style.position = 'fixed'
+    newElement.style.left = `${left}px`
+    newElement.style.bottom = `${bottom}px`
+    document.body.append(newElement)  
+}
+
+    newElement.addEventListener('click', function(){
+        newElement.remove()  
+   
+})
+
+}
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+newImage('assets/sword.png', 500, 405)
+
+
+
