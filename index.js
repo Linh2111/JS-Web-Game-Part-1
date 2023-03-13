9c53c0ae8dbf0ecd4b542290bff6eb27e0be88a9
Linh2111 <120156683+Linh2111@users.noreply.github.com>
2023-03-12 18:48:13 -0700

    add functions with comments

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 
 
+// Create newImage function. newImage function have three parameters (image, left, bottom)
 function newImage (image, left, bottom) {
- 
+    //newElement is assign to document.createElement. document.createElement function creates elements.
     let newElement = document.createElement('img')
+    //image is attribute. Pathway link goes here.
     newElement.src = image
+    //style of image
     newElement.style.position = 'fixed'
     newElement.style.left = `${left}px`
     newElement.style.bottom = `${bottom}px`
-    document.body.append(newElement)  
-
-
-    newElement.addEventListener('click', function(){
-        newElement.remove()  
-   
-})
-
+    //adds image to page
+    document.body.append(newElement)
+    //
+    return newElement
 }
 
+// created new function for newItems with three Parameters.
+function newItem(image, left, bottom){
+    //
+    let newElement = newImage (image, left, bottom)
+    //Removes image when users click from dom
+    newElement.addEventListener('dblclick', () => {
+        newElement.remove() 
+ 
+    })
+}
+
+// new images with three arguements listed 
 newImage('assets/green-character.gif', 100, 100)
 newImage('assets/tree.png', 200, 300)
 newImage('assets/pine-tree.png', 450, 200)
